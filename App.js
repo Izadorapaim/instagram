854cded8ad9eaaff7d38b13c86d88c3ced010c90
vagnerRubens <55748747+vagnerRubens@users.noreply.github.com>
2020-12-11 03:51:43 -0300

    Com botão de logout

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Feed from './src/pages/Feeds/index';
 import { createStackNavigator } from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
+import { AntDesign } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native'; 
 
 const Stack = createStackNavigator()
 
@@ -11,7 +13,18 @@ export default function App() {
     <View style={style.container}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Feed">
-          <Stack.Screen name="Feed" component={Feed} />
+          <Stack.Screen name="Feed" component={Feed}
+            options={{
+              headerTitleStyle: { fontFamily: 'Montserrat_700Bold' },
+              headerRight: () => (
+                <TouchableOpacity style= {{marginRight:10}}>
+                  <AntDesign name="logout"
+                    size={30}
+                    color="black"
+                  />
+                </TouchableOpacity>
+              )
+            }}/>
         </Stack.Navigator>
       </NavigationContainer>
     </View>
